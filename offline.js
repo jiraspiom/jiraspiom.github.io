1800d9c59b4e288f8e339b94d702a067017c0c69
jiraspiom <jiraspiom@yahoo.com.br>
2020-01-30 11:36:23 -0300

    adicionar arquivo

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1580394896,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledobistaculos.png",
+		"images/ninjajorge-sheet0.png",
+		"images/porta-sheet0.png",
+		"images/agua-sheet0.png",
+		"images/fundo01-sheet0.png",
+		"images/fundo02-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/tilemap.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
